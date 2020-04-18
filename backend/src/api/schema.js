1e8d008fbf6d78bef0c95706df9302c82b75c903
Narek Tumanyan <narek.t@simplytechnologies.net>
2020-04-18 20:29:33 +0400

    refactor(backend/graphql): automated schema loading

--- backend/src/api/schema.js
+++ backend/src/api/schema.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import glob from 'glob';
 
 const baseSchema = `
     type Query {
@@ -14,7 +15,14 @@ function joinSchemas(...schemas) {
   return [baseSchema, ...schemas].join('\n');
 }
 
-const querySchema = fs.readFileSync(path.join(__dirname, '/modules/user/user.graphql'), 'utf8');
-const schema = joinSchemas(querySchema);
+function loadSchema() {
+  return joinSchemas(
+    glob
+      .sync(path.join(__dirname, '/modules/**/*.graphql'))
+      .map((schemaPath) => fs.readFileSync(schemaPath, 'utf8')),
+  );
+}
+
+const schema = loadSchema();
 
 export default schema;
