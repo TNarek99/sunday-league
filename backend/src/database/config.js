aac4856e220ed96668e347dbbaca44d71c12a3cd
Narek Tumanyan <narek.t@simplytechnologies.net>
2020-04-26 02:23:08 +0400

    fix(database): fixed db connection configs

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    use_env_variable: 'DEV_DATABASE_URL',
     dialect: 'postgres',
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    use_env_variable: 'TEST_DATABASE_URL',
     dialect: 'postgres',
   },
-}
+};
