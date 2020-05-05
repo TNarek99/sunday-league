625567a41abd7145dcc17e4a238946d1ccab6e46
Narek Tumanyan <narek.t@simplytechnologies.net>
2020-05-05 14:53:28 +0400

    refactor(backend/context): throwing apollo error instance on auth fail

--- backend/src/api/context.js
+++ backend/src/api/context.js
@@ -1,11 +1,17 @@
+import { AuthenticationError } from 'apollo-server';
 import authService from '../modules/user/services/auth.service';
 import userService from '../modules/user/services/user.service';
+import { MESSAGE_UNAUTHENTICATED } from './messages';
 
 async function context({ req }) {
-  const firebaseToken = req.headers.authorization;
-  const { uid, phone_number: phoneNumber } = await authService.verifyIdToken(firebaseToken);
-  const user = await userService.getUserByFirebase(uid, phoneNumber);
-  return { user };
+  try {
+    const firebaseToken = req.headers.authorization;
+    const { uid, phone_number: phoneNumber } = await authService.verifyIdToken(firebaseToken);
+    const user = await userService.getUserByFirebase(uid, phoneNumber);
+    return { user };
+  } catch (e) {
+    throw new AuthenticationError(MESSAGE_UNAUTHENTICATED);
+  }
 }
 
 export default context;
