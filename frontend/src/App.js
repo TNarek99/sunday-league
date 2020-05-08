2b4104f4c06fb73968b0d5979b8cf8eeb759e952
Ea0011 <avagyanee@gmail.com>
2020-05-09 00:36:42 +0400

    chore: add temporary sign out button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import DomainRoutes from './routes/domain_routes';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase';
 
 const App = () => {
   return (
-    <Router>
-      <DomainRoutes />
-    </Router>
+    <>
+      <button onClick={() => firebase.auth().signOut()}>GET OUT</button>
+      <Router>
+        <DomainRoutes />
+      </Router>
+    </>
   );
 }
 
