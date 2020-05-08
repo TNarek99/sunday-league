1fb086cbc8bcfde069857500a27f47e41fdb3334
Ea0011 <avagyanee@gmail.com>
2020-05-08 23:26:33 +0400

    fix: current user status changes for sign in

--- frontend/src/pages/sign_in/sign_in.js
+++ frontend/src/pages/sign_in/sign_in.js
@@ -1,26 +1,34 @@
 import React, { useState, useContext } from 'react';
 import { useFirebaseAuth } from '../../api/services/firebase_auth';
 import UserContext from '../../contexts/authentication/user_context';
+import { Redirect } from 'react-router-dom';
 
 const SignIn = () => {
+  const { currentUser } = useContext(UserContext);
   const {
     signIn,
     confirmCode,
     signOut,
     isConfirming,
     errors
-  } = useFirebaseAuth('recaptcha-verifier');
-
-  const { currentUser } = useContext(UserContext);
+  } = useFirebaseAuth('recaptcha-verifier', currentUser.loaded && !currentUser.signedIn);
 
   const [phoneNumber, setPhoneNumber] = useState('');
   const [code, setCode] = useState('');
+
+  if (!currentUser.loaded) {
+    return <p>Loading...</p>
+  }
+
+  if (currentUser.signedIn) {
+    return <Redirect to='/' />;
+  };
 
   return (
     <div>
       {errors && <p>Something went wrong</p>}
       {
-        currentUser && currentUser.signedIn ?
+        currentUser && currentUser.loaded && currentUser.signedIn ?
           <div>
             <p>Already Signed In: Log out?</p>
             <button onClick={signOut}>Sign out</button>
