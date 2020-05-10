d3694e828b066d6ed8966e6edd771d226dc78af8
Ea0011 <avagyanee@gmail.com>
2020-05-10 21:36:02 +0400

    feat(input): add error reporting

--- frontend/src/components/input_with_label/input_with_label.js
+++ frontend/src/components/input_with_label/input_with_label.js
@@ -6,21 +6,30 @@ import './styles.css'
 const InputWithLabel = ({
   label,
   error,
+  showError,
   onChange,
   value,
+  ...rest
 }) => {
   return (
     <div className="input-with-label-container">
       <p className="label">{label}</p>
-      <input className="input" type="text" value={value} onChange={onChange} />
+      <input className={`input ${showError && error && 'input-error'}`} type="text" value={value} onChange={onChange} {...rest} />
+      {showError && error && <p className="error">{error}</p>}
     </div>
   )
 };
+
+InputWithLabel.defaultProps = {
+  showError: false,
+  error: '',
+}
 
 InputWithLabel.propTypes = {
   label: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   error: PropTypes.string,
+  showError: PropTypes.bool,
   value: PropTypes.string.isRequired,
 };
 
