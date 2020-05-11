893865e020914bbde409ebc71806d53898fcbc98
Ea0011 <avagyanee@gmail.com>
2020-05-11 22:38:05 +0400

    feat(games/list): add open games list and join functionality

--- frontend/src/api/services/games.js
+++ frontend/src/api/services/games.js
@@ -1,5 +1,6 @@
-import { useMutation } from "@apollo/react-hooks";
-import { CREATE_GAME } from "../mutations/games";
+import { useMutation, useLazyQuery } from "@apollo/react-hooks";
+import { CREATE_GAME, JOIN_GAME } from "../mutations/games";
+import { GET_OPEN_GAMES } from "../queries/games";
 
 export const useCreateGame = ({ onCompleted, onError }) => {
   const [createGame, { data }] = useMutation(CREATE_GAME, {
@@ -15,3 +16,22 @@ export const useCreateGame = ({ onCompleted, onError }) => {
     }
   }
 };
+
+export const useFetchOpenGames = ({ onCompleted, onError }) => {
+  const [fetchOpenGames, { data }] = useLazyQuery(GET_OPEN_GAMES, {
+    onCompleted,
+    onError,
+    fetchPolicy: 'network-only',
+  });
+
+  return { fetchOpenGames };
+};
+
+export const useJoinGame = ({ onCompleted, onError }) => {
+  const [joinGame, { data }] = useMutation(JOIN_GAME, {
+    onCompleted,
+    onError,
+  });
+
+  return { joinGame: (id) => joinGame({ variables: { id } }) };
+}
--- frontend/src/contexts/game/game_provider.js
+++ frontend/src/contexts/game/game_provider.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import GameContext from './game_context';
-import { useCreateGame } from '../../api/services/games';
+import { useCreateGame, useFetchOpenGames, useJoinGame } from '../../api/services/games';
 
 const GameProvider = ({ children }) => {
+  const [errors, setErrors] = useState({});
+  const [openGames, setOpenGames] = useState([]);
+  const [currentUserGames, setCurrentUserGames] = useState([]);
+
   const { createGame } = useCreateGame({
     onCompleted: () => { console.log('Game Created') },
     onError: () => { console.log('Error occured') },
   });
 
+  const { fetchOpenGames } = useFetchOpenGames({
+    onCompleted: ({ openGames }) => setOpenGames(openGames),
+    onError: ({ error }) => { setErrors({}) },
+  });
+
+  const { joinGame } = useJoinGame({
+    onCompleted: () => { },
+    onError: (errors) => setErrors({ join: errors.message }),
+  });
+
   return (
     <GameContext.Provider value={{
-      createGame
+      createGame,
+      openGames,
+      getOpenGames: fetchOpenGames,
+      joinGame,
+      currentUserGames,
     }}>
       {children}
     </GameContext.Provider>
