a9902ee2449804ca3eb1fe9b45f6d74279c2c3e0
Narek Tumanyan <narek.t@simplytechnologies.net>
2020-05-12 19:05:14 +0400

    feature(backend/user,team,game): added user->game->team->player->user graph APIs

--- backend/src/api/resolvers.js
+++ backend/src/api/resolvers.js
@@ -1,14 +1,22 @@
 import { combineResolvers } from 'graphql-resolvers';
 import { requiresToBeActive, requiresToBeNonActive } from './modules/authorization/resolvers';
-import { activateUserResolver, currentUserResolver, updateUserResolver } from './modules/user/resolvers';
+import {
+  activateUserResolver,
+  currentUserResolver,
+  updateUserResolver,
+  userCreatedGamesResolver,
+} from './modules/user/resolvers';
 import {
   openGamesResolver,
   createGameResolver,
   joinGameResolver,
   updateMatchStatusResolver,
   updateGameResolver,
+  gameFirstTeamResolver,
+  gameSecondTeamResolver,
 } from './modules/game/resolvers';
 import { DateTimeResolver, EmailResolver } from './modules/customTypes/resolvers';
+import { teamPlayersResolver, playerUserResolver } from './modules/team/resolvers';
 
 
 const resolvers = {
@@ -26,6 +34,19 @@ const resolvers = {
   },
   DateTime: DateTimeResolver,
   Email: EmailResolver,
+  User: {
+    createdGames: userCreatedGamesResolver,
+  },
+  Game: {
+    firstTeam: gameFirstTeamResolver,
+    secondTeam: gameSecondTeamResolver,
+  },
+  Team: {
+    players: teamPlayersResolver,
+  },
+  Player: {
+    user: playerUserResolver,
+  },
 };
 
 export default resolvers;
