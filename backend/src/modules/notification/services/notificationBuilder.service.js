b9bdabd06f8ce84b4fbbf7a8f2f92138953e8509
Narek Tumanyan <narek.t@simplytechnologies.net>
2020-05-13 22:49:17 +0400

    feature(backend/modules/notification): created gameDateChanged and invitation notification builders along with dependencies in other services

--- backend/src/modules/notification/services/notificationBuilder.service.js
+++ backend/src/modules/notification/services/notificationBuilder.service.js
@@ -1,23 +1,37 @@
 import notificationService from './notification.service';
 import gameService from '../../game/services/game.service';
-import teamService from '../../team/services/team.service';
-import { generateGameMatchStatusUpdatedMessage, userToNotificationDataForMessage } from '../utils/notificationBuilder.utils';
+import invitationService from '../../scheduler/services/invitation.service';
+import {
+  generateGameMatchStatusUpdatedMessage,
+  userToNotificationDataForMessage,
+  generateGameDateChangedMessage,
+  generateInvitationMessage,
+} from '../utils/notificationBuilder.utils';
 
 class NotificationBuilderService {
   async createGameMatchStatusChangedNotifications(game) {
-    const firstTeam = await gameService.getFirstTeam(game);
-    const secondTeam = await gameService.getSecondTeam(game);
-    const firstTeamPlayers = await teamService.getPlayers(firstTeam);
-    const secondTeamPlayers = await teamService.getPlayers(secondTeam);
+    const { firstTeamPlayers, secondTeamPlayers } = await gameService.getPlayers(game);
     const players = [...firstTeamPlayers, ...secondTeamPlayers];
     const message = generateGameMatchStatusUpdatedMessage(game);
     const notificationsData = players.map(userToNotificationDataForMessage(message));
     return notificationService.createNotifications(notificationsData);
   }
 
-  async createGameDateChangedNotification() {}
+  async createGameDateChangedNotification(game) {
+    const { firstTeamPlayers, secondTeamPlayers } = await gameService.getPlayers(game);
+    const players = [...firstTeamPlayers, ...secondTeamPlayers];
+    const message = generateGameDateChangedMessage(game);
+    const notificationsData = players.map(userToNotificationDataForMessage(message));
+    return notificationService.createNotifications(notificationsData);
+  }
 
-  async createInvitationNotification() {}
+  async createInvitationNotification(invitation) {
+    const user = await invitationService.getUser(invitation);
+    const game = await invitationService.getGame(invitation);
+    const message = generateInvitationMessage(game);
+    const notificationData = userToNotificationDataForMessage(message)(user);
+    return notificationService.createNotification(notificationData);
+  }
 }
 
 const notificationBuilderService = new NotificationBuilderService();
--- backend/src/modules/notification/utils/notificationBuilder.utils.js
+++ backend/src/modules/notification/utils/notificationBuilder.utils.js
@@ -1,3 +1,13 @@
-export const userToNotificationDataForMessage = (message) => (user) => ({ userId: user.id, message });
+import moment from 'moment';
 
-export const generateGameMatchStatusUpdatedMessage = (game) => `Match status for game ${game.id} changed to ${game.matchStatus}`;
+export const userToNotificationDataForMessage = (message) =>
+  (user) => ({ userId: user.id, message });
+
+export const generateGameMatchStatusUpdatedMessage = (game) =>
+  `Match status for game ${game.id} changed to ${game.matchStatus}`;
+
+export const generateGameDateChangedMessage = (game) =>
+  `Match date for game ${game.id} changed to ${moment(game.date).format('LLLL')}`;
+
+export const generateInvitationMessage = (game) =>
+  `You have been invited to game ${game.id} at ${moment(game.date).format('LLLL')}`;
